7a66833a7df03fb8816c0aed2a4ba6aadcbc1b16
Samuel Lebeau <samuel.lebeau@gmail.com>
2008-09-16 00:36:16 +0200

    Better class tests...

--- test/class_test.js
+++ test/class_test.js
@@ -1,3 +1,8 @@
+/**
+ * Simple test/example to demonstrate Mixin abstraction.
+**/
+
+// a Comparable object is an object that implements "compareTo"
 var Comparable = new Mixin({
   greaterThan: function(other) {
     return this.compareTo(other) > 0;
@@ -7,6 +12,7 @@ var Comparable = new Mixin({
     return this.compareTo(other) < 0;
   },
   
+  // Default equalTo implementation
   equalTo: function(other) {
     return this.compareTo(other) == 0;
   }
@@ -16,11 +22,10 @@ var Comparable = new Mixin({
 Number.addMethods({
   compareTo: function(number) {
     if (!Object.isNumber(number)) throw { type: "ArgumentError" };
-    if (this > number) return 1;
-    if (this < number) return -1;
-    return 0
+    return this - number;
   },
   
+  // specialized/faster equalTo implementation
   equalTo: function(number) {
     return this === number;
   }
@@ -34,17 +39,21 @@ var Set = Class.create(Enumerable, {
     $A(elements).each(this.add, this);
   },
   
+  _each: function(callback) {
+    this.elements.each(callback);
+  },
+  
   add: function(element) {
     if (this.elements.include(element))
       return false;
     this.elements.push(element);
-  },
+  }
 });
 
 // An Enumerable whose elements are Comparable...
 var Sortable = new Mixin(Enumerable, {
   sort: function() {
-    return this.toArray().sort(function(a, b) { return a.compareTo(b) });
+    return this.sortBy(function(a, b) { return a.compareTo(b) });
   }
 });
 
@@ -63,9 +72,16 @@ new Test.Unit.Runner({
   },
   
   testClassWithMixin: function() {
-    this.assert((3).lowerThan(4));
+    this.assert( (3).lowerThan(4) );
     this.assertNothingRaised(function() {
-      this.assert(!(3).equalTo("3"), "mixin shoudn't override class methods");
+      // remember we just added typecheck on NumberSet#add,
+      // just checking Comparable didn't override this method !
+      this.assert(!(3).equalTo("3"), "mixin shoudn't override prototype methods");
     }.bind(this));
+  },
+  
+  testNumberSet: function() {
+    var sortableSet = new NumberSet([3, 2, 2, 2, 1]);
+    this.assertEnumEqual([1, 2, 3], sortableSet.sort());
   }
 });
